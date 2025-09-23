5a4bd9c491978be6205ac176654e369a9ce84443
Rudney Oliveira <rudneymo@gmail.com>
2025-09-23 01:11:12 -0300

    Recomeço

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
-let listaAmigos = [];//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigoListado = listarAmigo();
-let totalAmigos = 0;
-
-//listarAmigo()
-//sortearAmigo()
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
